8f88d772a9174e4dab2dd508f86d2260029d5dad
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-20 16:28:47 +0200

    WordSpec gebruikt

--- src/test/scala-2.11/ConsTest.scala
+++ src/test/scala-2.11/ConsTest.scala
@@ -1,117 +1,132 @@
-import org.scalatest.{Matchers, GivenWhenThen, FlatSpec}
+import org.scalatest.{WordSpec, FunSpec, Matchers, FlatSpec}
 import List._
 
-class ConsTest extends FlatSpec with GivenWhenThen with Matchers {
-  it should "contain after add" in {
-    val list = Cons(5, Cons(3, Nil))
-    val result = list add 7
+class ConsTest extends WordSpec with Matchers {
+  "A list" when {
+    "adding" should {
+      "contain new item" in {
+        val list = Cons(5, Cons(3, Nil))
+        val result = list add 7
 
-    assert(result contains 7)
-    assert(result contains 5)
-    assert(result contains 3)
-    assert(!(result contains 4))
-  }
+        result contains 7 should equal (true)
+        result contains 5 should equal (true)
+        result contains 3 should equal (true)
+        result contains 4 should equal (false)
+      }
 
-  it should "add at the end" in {
-    val list = Cons(3, Nil)
-    val result = list add 5
+      "add at the end" in {
+        val list = Cons(3, Nil)
+        val result = list add 5
 
-    assert(result.toString() == "3 - 5")
-  }
+        result.toString should equal("3 - 5")
+      }
 
-  it should "contain 1 after add on Nil" in {
-    val list = Nil add 1
+      "contain 1 after add on Nil" in {
+        val list = Nil add 1
 
-    assert(list.toString == "1")
-  }
+        list.toString should equal("1")
+      }
+    }
 
-  it should "contain doubled values after map" in {
-    val list = Cons(5, Cons(3, Nil))
-    val result = list map (x => x * 2)
+    "mapping" should {
+      "contain doubled values after map" in {
+        val list = Cons(5, Cons(3, Nil))
+        val result = list map (x => x * 2)
 
-    assert(result contains 10)
-    assert(result contains 6)
-    assert(!(result contains 5))
-    assert(!(result contains 3))
-  }
+        result contains 10 should equal(true)
+        result contains 6 should equal(true)
+        result contains 5 should not equal true
+        result contains 3 should not equal true
+      }
+    }
 
-  it should "contain values after union" in {
-    val list = Cons(5, Cons(3, Nil))
-    val result = list union Cons(9, Cons(7, Nil))
+    "union" should {
+      "contain values after union" in {
+        val list = Cons(5, Cons(3, Nil))
+        val result = list union Cons(9, Cons(7, Nil))
 
-    assert(result contains 9)
-    assert(result contains 7)
-    assert(result contains 5)
-    assert(result contains 3)
-  }
+        result contains 9 should equal(true)
+        result contains 7 should equal(true)
+        result contains 5 should equal(true)
+        result contains 3 should equal(true)
+      }
 
-  it should "contain unique values after union" in {
-    val list1 = Cons(5, Nil)
-    val list2 = Cons(5, Nil)
-    val result = list1 union list2
+      "contain unique values after union" in {
+        val list1 = Cons(5, Nil)
+        val list2 = Cons(5, Nil)
+        val result = list1 union list2
 
-    assert(result.toString == "5")
-  }
+        result.toString should equal("5")
+      }
 
-  it should "contain original values after union" in {
-    val list1 = Cons(3, Cons(3, Nil))
-    val list2 = Cons(5, Nil)
-    val result = list1 union list2
+      "contain original values after union" in {
+        val list1 = Cons(3, Cons(3, Nil))
+        val list2 = Cons(5, Nil)
+        val result = list1 union list2
 
-    assert(result.toString == "3 - 3 - 5")
-  }
+        result.toString should equal("3 - 3 - 5")
+      }
 
-  it should "contain duplicates from both lists" in {
-    val list1 = Cons(3, Cons(3, Nil))
-    val list2 = Cons(5, Cons(5, Nil))
+      "contain duplicates from both lists" in {
+        val list1 = Cons(3, Cons(3, Nil))
+        val list2 = Cons(5, Cons(5, Nil))
 
-    val result = list1 union list2
+        val result = list1 union list2
 
-    assert(result.toString == "3 - 3 - 5 - 5")
-  }
+        result.toString should equal("3 - 3 - 5 - 5")
+      }
+    }
 
-  it should "write empty for an empty list" in {
-    assert(Nil.toString() == "empty list")
-  }
+    "converting to string" should {
 
-  it should "write 1 for a list of one item" in {
-    assert(Cons(1, Nil).toString() == "1")
-  }
+      "write empty for an empty list" in {
+        Nil.toString() should equal("empty list")
+      }
 
-  it should "write 1 - 2 for a list of two items" in {
-    assert(Cons(1, Cons(2, Nil)).toString() == "1 - 2")
-  }
+      "write 1 for a list of one item" in {
+        Cons(1, Nil).toString() should equal("1")
+      }
 
-  it should "concatenate items with the :: operator" in {
-    val list = Cons(1, Nil).::(1)
-    assert(list.toString() == "1 - 1")
-  }
+      "write 1 - 2 for a list of two items" in {
+        Cons(1, Cons(2, Nil)).toString() should equal("1 - 2")
+      }
+    }
 
-  it should "implicit convert items with the :: operator" in {
-    val list: List[Int] = 1 :: 2
-    assert(list.toString() == "1 - 2")
-  }
+    "adding with the :: operator" should {
+      "concatenate items with the :: operator" in {
+        val list = Cons(1, Nil).::(1)
+        list.toString() should equal("1 - 1")
+      }
 
-  it should "concatenate multiple values in right order" in {
-    val list: List[Int] = 1 :: 2 :: 3
-    assert(list.toString == "1 - 2 - 3")
-  }
+      "implicit convert items with the :: operator" in {
+        val list: List[Int] = 1 :: 2
+        list.toString() should equal("1 - 2")
+      }
 
-  it should "be empty when reverse empty" in {
-    assert((Nil reverse) == Nil)
-  }
+      "concatenate multiple values in right order" in {
+        val list: List[Int] = 1 :: 2 :: 3
+        list.toString should equal("1 - 2 - 3")
+      }
+    }
 
-  it should "be same when single item" in {
-    val list = Cons(1, Nil)
-    val result = list reverse
+    "reverse" should {
+      "be empty when reverse empty" in {
+        Nil reverse() should equal(Nil)
+      }
 
-    assert(result.toString == "1")
-  }
+      "be same when single item" in {
+        val list = Cons(1, Nil)
+        val result = list reverse()
 
-  it should "be reversed for more items" in {
-    val list = Cons(1, Cons(2, Nil))
-    val result = list reverse
+        result.toString should equal("1")
+      }
 
-    assert(result.toString == "2 - 1")
+      "be reversed for more items" in {
+        val list = Cons(1, Cons(2, Nil))
+        val result = list reverse()
+
+        result.toString should equal("2 - 1")
+      }
+    }
   }
 }
